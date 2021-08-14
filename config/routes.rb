25c81ae8a16f6a3f7919ed4b5a687b46969296f9
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-08-13 20:34:11 -0400

    Clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get 'home/index'
   root :to => "home#index"
   post "questions/:id", to: "questions#check_answer"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
