92bf938e80450232cf5fee083fe95f0d092604a7
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-08-13 20:29:38 -0400

    All new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_030335) do
+ActiveRecord::Schema.define(version: 2021_08_14_002104) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_08_13_030335) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "answered"
+    t.integer "quiz_id"
+    t.index ["quiz_id"], name: "index_questions_on_quiz_id"
   end
 
   create_table "quizzes", force: :cascade do |t|
