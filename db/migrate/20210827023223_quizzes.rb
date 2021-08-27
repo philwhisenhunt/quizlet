dabd1de2e9db3e647f8d72fbc791e1329d97ab26
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-08-27 17:04:53 -0400

    Make a table with quizzes and questions

--- db/migrate/20210827023223_quizzes.rb
+++ db/migrate/20210827023223_quizzes.rb
@@ -1,4 +1,8 @@
 class Quizzes < ActiveRecord::Migration[6.0]
   def change
+    create_table :quizzes_questions, :id => false do |t|
+      t.integer :quiz_id
+      t.integer :question_id
+    end
   end
 end
