95b64208c98adab15818bc3b6b2f275cbd2c067a
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-09-11 19:59:40 -0400

    Build out answers table migration

--- db/migrate/20210911024921_create_answers_table.rb
+++ db/migrate/20210911024921_create_answers_table.rb
@@ -1,6 +1,8 @@
 class CreateAnswersTable < ActiveRecord::Migration[6.0]
   def change
     create_table :answers_tables do |t|
+      add column :question, :answer, :string
+      add_index :question, :answer
     end
   end
 end
