50d5765e2a00538969b16eabc51dc895b2d9d229
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-09-12 21:20:37 -0400

    Refine migration

--- db/migrate/20210911024921_create_answers_table.rb
+++ db/migrate/20210911024921_create_answers_table.rb
@@ -1,8 +1,8 @@
 class CreateAnswersTable < ActiveRecord::Migration[6.0]
   def change
     create_table :answers_tables do |t|
-      add column :question, :answer, :string
-      add_index :question, :answer
+      add_column :questions, :answer, :string
+      add_index :questions, :answer
     end
   end
 end
