d8de5eba25c42543bdaca5b5cd4f332a102ae83c
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-09-14 20:36:01 -0400

    Restart

--- db/migrate/20210911024921_create_answers_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnswersTable < ActiveRecord::Migration[6.0]
-  def change
-    create_table :answers_tables do |t|
-      add_column :answers, :question, :string
-      add_column :answers, :answer, :string
-      add_index :answers, :answer
-    end
-  end
-end
