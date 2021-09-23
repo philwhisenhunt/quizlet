5764b44b7c67f48fcdd45b00d6e65c1d967e0969
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-09-23 19:02:06 -0400

    More precise naming for path

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post "questions/:id", to: "questions#check_answer"
   get 'start', to: "quizzes#start"
   get 'reset', to: "quizzes#reset"
-  get "quizzes/:id/build", to: "quizzes#build", as: "build"
+  get "quizzes/:id/build", to: "quizzes#build", as: "build_quiz"
 end
