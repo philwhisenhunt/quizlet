61c9807574b46b015aaa469bd8355ab2cfd0ec59
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-09-27 20:47:11 -0400

    Tried a bunch of ways to adjust that column

---
+++ db/migrate/20210928003703_adjust_questions_table.rb
@@ -0,0 +1,9 @@
+class AdjustQuestionsTable < ActiveRecord::Migration[6.0]
+  def change
+    # change_column :questions, :title, null: false
+    # change_column :questions, :answer, null: false
+    # change_column_null :questions, :title, false
+    # change_column_null :questions, :answer, false
+
+  end
+end
