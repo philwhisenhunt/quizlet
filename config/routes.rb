e19e9355f8d8a638283395272d8e62c8b7c14f85
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-10-06 21:14:44 -0400

    Set more natural route
    Don't let the id get in the way

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :quizzes
+  # resources :quizzes
   resources :questions
   get 'home/index'
   root :to => "home#index"
   post "questions/:id", to: "questions#check_answer"
-  get 'quizzes/:id/start', to: "quizzes#start", as: "start"
+  get 'quizzes/start', to: "quizzes#start", as: "start"
   get 'reset', to: "quizzes#reset"
   get "quizzes/:id/build", to: "quizzes#build", as: "build_quiz"
   get "complete", to: "quizzes#complete"
