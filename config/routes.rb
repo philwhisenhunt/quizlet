bff5d2358f93978c245f84c832671574d9c2c707
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-10-11 21:39:03 -0400

    Set the way it should be

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # resources :quizzes
+  resources :quizzes
   resources :questions
   get 'home/index'
   root :to => "home#index"
-  post "questions/:id", to: "questions#check_answer"
+  # post "questions/:id", to: "questions#check_answer"
   get 'quizzes/start', to: "quizzes#start", as: "start"
   get 'reset', to: "quizzes#reset"
   get "quizzes/:id/build", to: "quizzes#build", as: "build_quiz"
