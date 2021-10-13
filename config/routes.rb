18195774c1e0c3c2aa7a734b528e8bff4b91f60a
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-10-12 22:04:17 -0400

    Make the correct questions show up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :questions
   get 'home/index'
   root :to => "home#index"
-  # post "questions/:id", to: "questions#check_answer"
+  post "questions/:id", to: "questions#check_answer"
   get 'quizzes/start', to: "quizzes#start", as: "start"
   get 'reset', to: "quizzes#reset"
   get "quizzes/:id/build", to: "quizzes#build", as: "build_quiz"
