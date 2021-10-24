cba1cc1a64824157ea847d215111b755a123c5d0
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-10-23 21:34:10 -0400

    Redirect to the quiz spot

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,10 +24,10 @@ class QuestionsController < ApplicationController
   def create
     # byebug
     @question = Question.new(question_params)
-
+    @quiz = Quiz.find(question_params[:quiz_id])
     respond_to do |format|
       if @question.save
-        format.html { redirect_to questions_path, notice: "Question was successfully created." }
+        format.html { redirect_to quiz_path(@quiz), notice: "Question was successfully created." }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new, status: :unprocessable_entity }
