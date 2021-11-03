955a0a6be01e663b3c4d97428be27cd436e5f4c6
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-11-02 22:20:44 -0400

    Add handle_answer action

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -106,6 +106,10 @@ class QuizzesController < ApplicationController
 
   end
 
+  def handle_answer
+    byebug
+    @attempted_answer = params[:attempted_answer]
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_quiz
