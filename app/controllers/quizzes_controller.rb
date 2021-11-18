eb9be09783729e59e267f9ea83e6f7c45b1b2f4c
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-11-17 21:23:37 -0500

    Add error message

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -118,7 +118,7 @@ class QuizzesController < ApplicationController
       format.html { redirect_to quiz_path(@quiz), notice: "Question was successfully created under #{@quiz.name}"}
       # format.json {render :show, status: :ok, location: @quiz}
     else
-      format.html {redirect_to quiz_path(@quiz) }
+      format.html {redirect_to quiz_path(@quiz), notice: "Question was not saved." }
      
     end
   end
