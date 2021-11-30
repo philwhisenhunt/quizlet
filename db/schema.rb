ba1c8660875b64f13d51f00f0f88d5f97f7fd769
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-11-29 20:45:00 -0500

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_233158) do
+ActiveRecord::Schema.define(version: 2021_11_30_014006) do
 
   create_table "attempted_answers", force: :cascade do |t|
     t.string "attempted_answer"
     t.boolean "correct_answer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "question_id"
   end
 
   create_table "questions", force: :cascade do |t|
