39f17958f43b8104aa082fbd435bcad3d3ac2f54
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-12-20 17:56:56 -0500

    Hard code that first quiz for now

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root :to => "home#index"
   post "questions/:id", to: "questions#check_answer"
-  get 'quizzes/start', to: "quizzes#start", as: "start"
+  get 'quizzes/:id/start', to: "quizzes#start", as: "start"
   get 'reset', to: "quizzes#reset"
   get "quizzes/:id/build", to: "quizzes#build", as: "build_quiz"
   get "quizzes/:id/start", to: "quizzes#start", as: "start_quiz"
