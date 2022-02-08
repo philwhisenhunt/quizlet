3d376117071c9831a60b041ae1731820e14b8c55
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-02-08 07:24:06 -0500

    Shift routes to per quiz

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post "questions/:id", to: "questions#check_answer"
   get 'reset', to: "quizzes#reset"
   get "quizzes/:id/build", to: "quizzes#build", as: "build_quiz"
-  get "complete", to: "quizzes#complete"
+  get "quizzes/:id/complete", to: "quizzes#complete", as: "complete"
   get "quizzes/:id/session_maker", to: "quizzes#session_maker", as: "session_maker"
   get "quizzes/:id/add_question_to_quiz", to: "quizzes#add_question_to_quiz", as: "add_question_to_quiz"
   post "quizzes/:id/session_maker", to: "quizzes#handle_answer"
