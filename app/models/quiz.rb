da54cfdb8e1289f6b3fffef2c0e28a196feef94d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-02-16 20:55:34 -0500

    Add validation

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,7 @@ class Quiz < ApplicationRecord
     # has_many :quizzes_question
     after_create_commit {broadcast_prepend_to :quizzes}
     has_many :questions
-    # validates_presence_of :name
+    validates_presence_of :name
 
     def gather_questions
     end
